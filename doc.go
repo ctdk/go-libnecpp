94a744d4cad49db45c3ec97453f61e379756b645
ctdk <jbingham@gmail.com>
2017-03-29 13:13:50 -0700

    Package should be capitalized in doc.go

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 /*
-package necpp is a Go wrapper around libnecpp, which is a C library for
+Package necpp is a Go wrapper around libnecpp, which is a C library for
 nec++, which is a C++ rewrite of nec2c, which is itself a C rewrite/translation
 of the original FORTRAN code for NEC2, an antenna modelling package.
 
